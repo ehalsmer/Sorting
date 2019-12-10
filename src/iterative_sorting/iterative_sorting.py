d717a742c37e8b74c17c5f62132f5cc700650329
Eleasah Halsmer <eleasahjh@gmail.com>
2019-12-10 16:32:39 -0700

    wip bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,35 +5,25 @@ def selection_sort( arr ):
         # Find min of unsorted subarray
         current_min = min(arr[i:])
         # Do the swap
-        minFound = arr.index(current_min)
+        min_index = arr.index(current_min)
         temp = arr[i]
         arr[i] = current_min
-        arr[minFound] = temp
-        print(arr)
-        # cur_index = i
-        # print('cur_index', cur_index)
-        # smallest_index = cur_index
-
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-
-
-
-
-        # TO-DO: swap
-
-
-
-
+        arr[min_index] = temp
+        # print(arr)
     return arr
 
-selection_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7])
+# selection_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7])
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for i in range(1, len(arr) - 1):
+        print(i)
+        if arr[i] < arr[i - 1]:
+            # swap:
+            print(i, i-1)
     return arr
 
+print(bubble_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
