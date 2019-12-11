f83a8998ce3d48990118452afa4a752f74fad5a4
Eleasah Halsmer <eleasahjh@gmail.com>
2019-12-11 14:55:57 -0700

    merge function complete

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,22 +1,34 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
-    longest = len(arrA) if len(arrA) >= len(arrB) else len(arrB)
-    while len(arrA) != 0 and len(arrB) != 0:
-        for i in range(0, longest-1):
-            print(i)
-            # if arrA[i] < arrB[i]:
-                # merged_arr.append(arrA[i])
-                # print(merged_arr)
-                # arrA.remove(f'{arrA[i]}')
-            # elif arrB[i] < arrA[i]:
-                # merged_arr.append(arrB[i])
-                # arrB.remove(f'{arrB[i]}')
+    # elements = len( arrA ) + len( arrB )
+    merged_arr = []
+    # Three indices, to keep track of position in arrA, arrB, and merged_arr
+    i = 0
+    j = 0
+    k = 0
+    # compare arrA[i] and arrB[j]. Append smaller element onto merged_arr and increment source array's index.
+    while i < len(arrA) and j < len(arrB):
+        if arrA[i] < arrB[j]:
+            merged_arr.append(arrA[i])
+            i += 1
+        elif arrB[j] < arrA[i]:
+            merged_arr.append(arrB[j])
+            j += 1
+        # in case arrA[i] == arrB[j], append both elements and increment both i and j
+        else:
+            merged_arr.extend([arrA[i], arrB[j]])
+            i += 1
+            j += 1
+    # Once we've exhausted either arrA or arrB (if they're different lengths), append the remainder of the remaining array onto merged_arr:
+    if i < len(arrA):
+        merged_arr.extend(arrA[i:])
+    elif j < len(arrB):
+        merged_arr.extend(arrB[j:])
+    # while len(arrA) != 0 and len(arrB) != 0:
+
     return merged_arr
 
-print(merge([6,8,9,10],[1,2,3]))
+print(merge([1,6,8,9,10],[1,2,3,7,11]))
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
